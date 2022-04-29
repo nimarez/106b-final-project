d30198ac45be7db3beefda858901056835a14df4
Jaren Mendelsohn <jaren.mendelsohn@gmail.com>
2022-04-29 15:26:10 -0700

    Fixed random paths bug

--- pedestrian/controllers/ccpp/planner.py
+++ pedestrian/controllers/ccpp/planner.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 from dynamic_objects import DynamicObject, Human, Projectile
 
 
@@ -44,29 +45,43 @@ class Planner(object):
         # Discretizes with the grid size equal to the robot size
         pass
 
-    def _generate_spanning_tree(self, large_map, start_pos):
+    def _generate_spanning_tree(self, large_map, start_pos, previous_tree=None):
         """
         Generates a (not minimal) spanning tree, returns a dictionary of (pos) --> [boolean for up down left right if it connects]
         large_map: Returned from _largify_cells, binary matrix for large cells for spanning tree algorithm
         start_pos: tuple of (x, y) starting position in large coordinates
-        NOTE: This should probably be improved to make the tree as straight as possible, but that's effort
+        NOTE: This is not deterministic, we can generate random trees each time
+        NOTE: Edges are only one way, from parent to child
         """
-        to_visit = [start_pos]
+        # TODO: Handle previous tree
+        # Previous tree should just be a line -- assert this before
+        # Add the first node to the stack and make sure that if it's in the previous tree, that edge is queued first
+        to_visit = [(None, None, start_pos)]
         edges = {}
         visited = set()
+        # Queue all nodes into to visit
+        # This might be enough? Except adds extra edges
+        # TODO Look into this
         while to_visit:
-            current = to_visit.pop()
+            parent, parent_index, current = to_visit.pop()
+            print("Visiting", current)
             if current in visited:
                 continue
+            if current not in edges:
+                edges[current] = [False, False, False, False]
+            if parent is not None:
+                edges[parent][parent_index] = True
             visited.add(current)
-            current_edges = [False, False, False, False]
-            for i, (xdiff, ydiff) in enumerate(self.coords_to_check):
+            c = list(zip(range(4), self.coords_to_check[:]))
+            random.shuffle(c)
+            for i, (xdiff, ydiff) in c:
                 newx, newy = current[0] + xdiff, current[1] + ydiff
-                if newx < 0 or newy < 0 or newx >= len(large_map[0]) or newy >= len(large_map) or large_map[newy][newx] == 1:
+                if newx < 0 or newy < 0 \
+                   or newx >= len(large_map[0]) or newy >= len(large_map) \
+                   or large_map[newy][newx] != 0 or (newx, newy) in visited:
                     continue
-                to_visit.append((newx, newy))
-                current_edges[i] = True
-            edges[current] = current_edges
+                to_visit.append((current, i, (newx, newy)))
+        print(edges)
         return edges
 
     def _largify_cells(self, extended_occupancy_map):
@@ -87,10 +102,23 @@ class Planner(object):
             result.append(row)
         return result
 
+    def _prune_tree(self, path, tree, current_position):
+        """
+        path: Returned from _walk_tree
+        tree: Returned from generated spanning tree
+        current_position: Current location in tree
+        Prunes the tree to only contain nodes which are partially visited
+        This is useful because we can mark the fully visited nodes as obstacles essentially
+            It costs nothing to go around them because the paths are all uniform length because space-filling
+        Also removes the unvisited squares, because we want to regenerate the path if we encounter a dynamic obstacle 
+        Returns pruned tree
+        """
+        # TODO
+        pass
+
     def _walk_tree(self, tree, start_pos):
         """
         Takes in a tree (in large coordinates) and start_pos (in small coordinates !!) and produces a path from walking that tree
-        TODO: Handle prefix
         """
         stack = [(start_pos[0] // 2, start_pos[1] // 2)]
         previous = None
@@ -121,22 +149,11 @@ class Planner(object):
         path = [start_pos]
         while stack:
             current = stack.pop()
-            #print("Running on", current)
             # Don't check for visited here because we're probably already visited
             visited.add(current)
             current_position = path[-1]
             xmod, ymod = current_position[0] % 2, current_position[1] % 2
-            shift = coord_check_shift[(xmod, ymod)]
-            shifted_coords = self.coords_to_check[shift:] + self.coords_to_check[:shift]
-            for unshifted_i, (xdiff, ydiff) in enumerate(shifted_coords):
-                index = (unshifted_i + shift) % 4
-                #print("Checking", ["up", "down", "left", "right"][index], "for", current_position)
-                new_pos = (current[0] + xdiff, current[1] + ydiff)
-                if tree[current][index] and new_pos not in visited:
-                    # We want to visit current against at the end, and repeat the same thing
-                    stack.append(current)
-                    stack.append(new_pos)
-                    break
+            print("Big square", current, "previously at", current_position)
             if previous is not None:
                 # TODO: This might fail when we have a tree of size 1 because previous is never set
                 # Finds the counterclockwise shortest path from the last position to the closest position in the new square
@@ -156,32 +173,52 @@ class Planner(object):
                         if new_position[0] // 2 == current[0] and new_position[1] // 2 == current[1]:
                             path.append(new_position)
                             break
+            # Get position again after moving into this square so we know which direction to go next
+            current_position = path[-1]
+            xmod, ymod = current_position[0] % 2, current_position[1] % 2
+            shift = coord_check_shift[(xmod, ymod)]
+            shifted_coords = self.coords_to_check[shift:] + self.coords_to_check[:shift]
+            for unshifted_i, (xdiff, ydiff) in enumerate(shifted_coords):
+                index = (unshifted_i + shift) % 4
+                print("Checking", ["up", "left", "down", "right"][index], "for", current_position)
+                print((xdiff, ydiff))
+                new_pos = (current[0] + xdiff, current[1] + ydiff)
+                if tree[current][index] and new_pos not in visited:
+                    # We want to visit current against at the end, and repeat the same thing
+                    stack.append(current)
+                    stack.append(new_pos)
+                    print("Ending")
+                    break
             previous = current
         return path
 
-    def _generate_spanning_tree_path(self, extended_occupancy_map, prefix, dt=0.5):
-        # Prefix should just be start pos for now
-        if len(prefix) != 1:
-            raise Exception("Todo")
-        start_pos = prefix[-1]
+    def generate_plan(self, extended_occupancy_map, start_pos, current_pos=None, previous_tree=None, dt=0.5):
+        """
+        Generates a path (dict of time to position) through an occupancy map
+        Extended occupancy map is same as occupancy map but with the potential for a square to be already explored
+        Dynamic objects is a representation of human intent, e.g. list of dict of time to coordinate
+        When starting out, current_pos and previous_tree are not needed
+            Can set these to regenerate part of the path
+        Thing to consider: we must complete the other side of squares we already visited first, if we decide to replan
+        - Solution: Replan given the current completed prefix of the plan,
+            record big squares as being fully cleaned, partially cleaned, etc.
+        Future work:
+        - Make spanning tree thing work for obstacles which only cover 1/4 of the mega cell
+        """
+        pruned_tree = None
+        if current_pos is not None:
+            # TODO: Update occupancy map with fully occupied stuff from previous run, should probably be done outside this function
+            # NOTE: Assumes that walk tree is deterministic (!!)
+            previous_path = self._walk_tree(previous_tree, start_pos)
+            pruned_tree = self._prune_tree(previous_path, previous_tree, current_position)
         large_cells = self._largify_cells(extended_occupancy_map)
-        tree = self._generate_spanning_tree(large_cells, start_pos)
+        tree = self._generate_spanning_tree(large_cells, start_pos, pruned_tree)
         path = self._walk_tree(tree, start_pos)
         # Assumes the robot moves with constant velocity of 1 square/dt
         plan = {}
         for i in range(len(path)):
             plan[dt*i] = path[i]
         return plan
-
-    def generate_plan(self, extended_occupancy_map, dynamic_objects, curr_plan):
-        # Extended occupancy map is same as occupancy map but with the potential for a square to be already explored
-        # Dynamic objects is a representation of human intent, e.g. list of dict of time to coordinate.
-        # Returns an array of squares to visit indexed by time. 
-        # Thing to consider: we must complete the other side of squares we already visited first, if we decide to replan
-        # - Solution: Replan given the current completed prefix of the plan, record big squares as being fully cleaned, partially cleaned, etc.
-        # Future work:
-        # - Make spanning tree thing work for obstacles which only cover 1/4 of the mega cell
-        pass
 
     def main(self, root_node, human_handle):
         occ = self.generate_occupancy_map(root_node)
@@ -211,28 +248,35 @@ def double_array(arr):
     return result
 
 if __name__ == "__main__":
+    #random.seed(0.41783174376782783)
     p = Planner(6)
 
-    # Uncomment to use standard 6x6 template
-    occ = np.array([[0, 0, 0, 1, 0, 0], [1, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0]])
+    #occ = np.array([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    occ = np.array([[0, 0, 0], [0, 0, 0]])
     occ = double_array(occ)
     start_pos = (0, 0)
 
-    human = Human((0, 0), (0, 8), 1)
-    human_traj = human.get_traj(occ, dt=10)
-    #print(human_traj)
-
-    # Uncomment to generate random occupancy
-    #prob = 0.95
-    #occ = np.random.choice([0, 1], size=(30, 30), p=[prob, 1 - prob])
-    #occ[0][0] = 0
+    # Uncomment to use standard 6x6 template
+    #occ = np.array([[0, 0, 0, 1, 0, 0], [1, 0, 0, 0, 0, 0], [1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 1, 1], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0]])
     #occ = double_array(occ)
     #start_pos = (0, 0)
+
+    #human = Human((0, 0), (0, 8), 1)
+    #human_traj = human.get_traj(occ, dt=10)
+
+    projectile = Projectile((3, 3), (1, 0.5))
+
+    # Uncomment to generate random occupancy
+    prob = 0.95
+    occ = np.random.choice([0, 1], size=(30, 30), p=[prob, 1 - prob])
+    occ[0][0] = 0
+    occ = double_array(occ)
+    start_pos = (0, 0)
 
     # Edge cases
     #occ = np.array([[1, 0, 1], [0, 0, 0], [1, 0, 1]])
     #occ = double_array(occ)
     #start_pos = (0, 2)
 
-    plan = p._generate_spanning_tree_path(occ, [start_pos])
-    p.visualize_plan(occ, [Projectile((3, 3), (1, 0.5))], plan)
+    plan = p.generate_plan(occ, start_pos)
+    p.visualize_plan(occ, [], plan, t_step=100000)
