987c644a0938c301ff37c2fb22955836b09b6f4e
Maxime Defauw <maxime.defauw@gmail.com>
2022-04-29 16:24:43 -0700

    timestep

--- pedestrian/controllers/ccpp/ccpp.py
+++ pedestrian/controllers/ccpp/ccpp.py
@@ -1,8 +1,10 @@
 from controller import Robot, Camera, Motor, Node, Supervisor
 from collections import namedtuple
 
+CURRENT_TIME = 0
 
 class CCPPController(Supervisor):
+    
     def __init__(self):
         super(CCPPController, self).__init__()
         self.timeStep = 64  # set the control time step
@@ -15,11 +17,11 @@ class CCPPController(Supervisor):
         # right motor
         self.right_motor = self.getDevice("right wheel motor")
         self.right_motor.setPosition(float('inf'))
-        self.right_motor.setVelocity(6.67)
+        #self.right_motor.setVelocity(6.67)
         # left motor
         self.left_motor = self.getDevice("left wheel motor")
         self.left_motor.setPosition(float('inf'))
-        self.left_motor.setVelocity(6.67)
+        #self.left_motor.setVelocity(6.67)
         
         Box = namedtuple('Box', ['translation', 'rotation', 'size'])
         
@@ -36,11 +38,21 @@ class CCPPController(Supervisor):
                 size = box_obj.getField("size").getSFVec3f()
                 boxes.append(Box(trans, rot, size))
             
-          
+    time_arr = [0, 1280, 2560, 3840, 5120, 6400]
+    poss_arr = [(1.49, 0), (1.7, 0.5), (1, 0.5), (0.6, 0.5), (1, 1), (1.7, 1.3)]
+    
+    def turutlebot_movement():
+        # move the turtlebot
+        
+        
+        
+        
     def run(self):
+        global CURRENT_TIME
         # main control loop: perform simulation steps of 32 milliseconds
         # and leave the loop when the simulation is over
-        while self.step(self.timeStep) != -1: 
+        while self.step(self.timeStep) != -1:
+            CURRENT_TIME += self.timeStep
             if self.getSupervisor():
                 position = self.robot.getPosition()
                 if (4.95 < position[0] ** 2 + position[1] ** 2 < 5.05):
